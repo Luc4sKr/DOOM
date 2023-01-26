dd01f87e84f7f60f197307c835f1c90b36791a5f
Lucas <lucaskreuch77@gmail.com>
2023-01-26 10:13:34 -0300

    Ray casting npc

--- sound.py
+++ sound.py
@@ -8,4 +8,11 @@ class Sound:
         self.game = game
         self.path = "resources/sound/"
 
-        self.shotgun = pygame.mixer.Sound(f"{self.path}/shotgun.wav")
+        self.shotgun = pygame.mixer.Sound(self.path + 'shotgun.wav')
+        self.npc_pain = pygame.mixer.Sound(self.path + 'npc_pain.wav')
+        self.npc_death = pygame.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_shot = pygame.mixer.Sound(self.path + 'npc_attack.wav')
+        self.npc_shot.set_volume(0.2)
+        self.player_pain = pygame.mixer.Sound(self.path + 'player_pain.wav')
+        self.theme = pygame.mixer.music.load(self.path + 'theme.mp3')
+        pygame.mixer.music.set_volume(0.4)
